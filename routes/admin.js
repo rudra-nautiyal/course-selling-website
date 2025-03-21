5bb924a6f83d8dcbf447a675c85d1295fab56636
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-03-21 12:10:03 +0530

    adding auth end points

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,54 @@
 const { Router } = require("express");
+const { AdminModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = "admin_s3cret";
 
 const adminRouter = Router();
-const { AdminModel } = require("../db");
 
-adminRouter.post("/signup", function (req, res) {});
-adminRouter.post("/login", function (req, res) {});
+adminRouter.post("/signup", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body; // TODO: adding zod validation
+  // TODO: hash the passwordso plain pw is not stored in the db
+
+  try {
+    await AdminModel.create({
+      email,
+      password,
+      firstName,
+      lastName,
+    });
+  } catch (e) {
+    res.json("Incorrect inputs.");
+  }
+
+  res.json({
+    message: "You have signed up.",
+  });
+});
+adminRouter.post("/login", async function (req, res) {
+  const { email, password } = req.body;
+
+  // rehash the password and bcrypt the library.
+
+  const admin = await AdminModel.findOne({
+    email,
+    password,
+  });
+
+  if (admin) {
+    const token = jwt.sign(
+      {
+        id: user_id,
+      },
+      JWT_ADMIN_PASSWORD
+    );
+
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json("Incorrect Credentials");
+  }
+});
 
 adminRouter.post("/course", function (req, res) {});
 adminRouter.put("/course", function (req, res) {});
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,55 @@
 const { Router } = require("express");
+const { UserModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "user_s3cret";
 
 const userRouter = Router();
 
-userRouter.post("/signup", function (req, res) {});
-userRouter.post("/login", function (req, res) {});
+userRouter.post("/signup", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body; // TODO: adding zod validation
+  // TODO: hash the passwordso plain pw is not stored in the db
+
+  try {
+    await UserModel.create({
+      email,
+      password,
+      firstName,
+      lastName,
+    });
+  } catch (e) {
+    res.json("Incorrect inputs.");
+  }
+
+  res.json({
+    message: "You have signed up.",
+  });
+});
+
+userRouter.post("/login", async function (req, res) {
+  const { email, password } = req.body;
+
+  // rehash the password and bcrypt the library.
+
+  const user = await UserModel.findOne({
+    email,
+    password,
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user_id,
+      },
+      JWT_USER_PASSWORD
+    );
+
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json("Incorrect Credentials");
+  }
+});
 userRouter.get("/courses", function (req, res) {});
 
 module.exports = {
